1cef772dfd0b8b2029519ecbb07fbc0b8214f173
- <70720102+blairtyx@users.noreply.github.com>
2022-04-17 02:14:42 -0400

    update ip config method

---
+++ sensor_data_transmit/data_sender.cpp
@@ -0,0 +1,9 @@
+#include "mbed.h"
+#include "SocketAddress.h"
+#include "EthernetInterface.h"
+#include "mbed-trace/mbed_trace.h"
+
+#define TRACE_GROUP "Main"
+
+
+
--- sensor_data_transmit/main.cpp
+++ sensor_data_transmit/main.cpp
@@ -1,4 +1,3 @@
-#include "fnet_error.h"
 #include "mbed.h"
 #include "SocketAddress.h"
 #include "EthernetInterface.h"
@@ -21,7 +20,10 @@ int main()
     TCPSocket socket;
     char sbuffer[64];
 
-    ret = eth.set_network("192.168.3.39","255.255.255.0","192.168.3.1"); // static interface
+    // set the Ethernet Interface
+    ret = eth.set_network(MBED_CONF_APP_LOCAL_HOST,
+                        MBED_CONF_APP_LOCAL_MASK,
+                        MBED_CONF_APP_LOCAL_GATEWAY); // static interface
     if (ret != 0) {
         tr_error("Set Network error: %x", ret);
         goto end;
@@ -39,13 +41,12 @@ int main()
     }
     tr_info("The client IP address is '%s'\n", a.get_ip_address() ? a.get_ip_address() : "None");
 
-    // Open a socket on the network interface, and create a TCP connection to 192.168.4.1:4000
-    
+    // Open a socket on the network interface, and create a TCP connection to MBED_CONF_APP_SERVER_HOST
     socket.open(&eth);
 
     // eth.gethostbyname("raspberrypi.local", &a);
-    a.set_ip_address("192.168.50.206");
-    a.set_port(4000);
+    a.set_ip_address(MBED_CONF_APP_SERVER_HOST);
+    a.set_port(MBED_CONF_APP_SERVER_PORT);
     socket.connect(a);
     if (ret != 0){
         tr_error("No IP address");
@@ -53,10 +54,10 @@ int main()
     }
 
     // Send a simple request
-    for (int i=0; i<100000; i++){
+    for (int i=0; i<10; i++){
         sprintf(sbuffer, "Hi, im client No.%d\r\n", i);
         int scount = socket.send(sbuffer, sizeof sbuffer);
-        // printf("sent %d [%.*s]\n", scount, strstr(sbuffer, "\r\n") - sbuffer, sbuffer);
+        printf("sent %d [%.*s]\n", scount, strstr(sbuffer, "\r\n") - sbuffer, sbuffer);
     }
 
     // Close the socket to return its memory and bring down the network interface
