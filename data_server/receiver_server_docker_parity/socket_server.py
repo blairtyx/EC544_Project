79247818dfcfc43cfaf52c7516c901ab239b2bdb
- <70720102+blairtyx@users.noreply.github.com>
2022-05-02 02:22:31 -0400

    add result of demo-main-group

--- data_server/receiver_server_docker_parity/socket_server.py
+++ data_server/receiver_server_docker_parity/socket_server.py
@@ -1,41 +1,62 @@
 import socket
+import datetime
+import sqlite3
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Bind the socket to the port
-server_address = ("0.0.0.0", 4000)
-print('starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
+if __name__ == "__main__":
+    # init variables
+    counter = 0
+    auto_time = datetime.datetime.now()
+    print("[Server] Start server at: ", auto_time)
 
-# Listen for incoming connections
-sock.listen(1)
 
-counter = 0
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ("0.0.0.0", 4001)
+    print('[Server] Starting up on {} port {}'.format(*server_address))
+    sock.bind(server_address)
+    sock.listen(1)
 
-while True:
-    # Wait for a connection
-    print('waiting for a connection')
-    connection, client_address = sock.accept()
-    try:
-        print('connection from', client_address)
+    # Content databases
+    db_0 = sqlite3.connect("content_0.sqlite")
+    cur_0 = db_0.cursor()
+    cur_0.execute("CREATE TABLE IF NOT EXISTS experiments (num TEXT, content TEXT)")
+    db_0.commit()
 
-        # Receive the data in small chunks
-        while True:
-            data = connection.recv(256)
-            print("index:  {}".format(data[0:4]))
-            print('msg:    {}'.format(data[4:228]))
-            print('parity: {}'.format(data[228:244]))
-            if data:
-                # print('sending data back to the client')
-                # connection.sendall(str.encode("Im the server, cheers * {}".format(counter)))
-                counter += 1
-            else:
-                print("Received {} packets".format(counter))
-                print('no more data from', client_address)
-                counter = 0
-                break
+    # Parity database
+    db_parity = sqlite3.connect("parity_0.sqlite")
+    cur_p = db_parity.cursor()
+    cur_p.execute("CREATE TABLE IF NOT EXISTS test (num TEXT, parity TEXT)")
+    db_parity.commit()
+
+    # Connection
+    while True:
+        print('[Server] Waiting for a connection')
+        connection, client_address = sock.accept()
+        try:
+            start_time = datetime.datetime.now()
+            print('[Server] Connection from', client_address, 'start at: ', start_time)
+            # Receive data 
+            while True:
+                data = connection.recv(256)
+                if data:
+                    print("number: {}".format(data[0:4].decode()))
+                    print('content:{}'.format(data[4:228].decode()))
+                    print('parity: {}'.format(data[229:245].hex())) # one byte of terminal indicator
+                    cur_0.execute("INSERT INTO experiments (num, content) values (\"{}\", \"{}\")".format(data[0:4].decode(), data[4:228].decode()))
+                    db_0.commit()
+                    cur_p.execute("INSERT INTO test (num, parity) values (\"{}\", \"{}\")".format(data[0:4].decode(), data[229:245].hex()))
+                    db_parity.commit()
+                    counter += 1
+                else:
+                    print("Received {} packets".format(counter))
+                    print('no more data from', client_address)
+                    counter = 0
+                    break
+                
+        finally:
+            # Clean up the connection
+            connection.close()
+            db_0.close()
+            db_parity.close()
             
-    finally:
-        # Clean up the connection
-        connection.close()
