66893243ab31df7eb87d918c51507f6c2c559a4e
xph3120 <80809231+xph3120@users.noreply.github.com>
2022-05-03 01:16:28 -0400

    server handles data and gives animation feedback

---
+++ data_server/animation.py
@@ -0,0 +1,119 @@
+frame1 = \
+"                              Straighten your neck!!!           \n" + \
+"                                       .                        \n" + \
+"                                     `:++/:.                    \n" + \
+"                                    `+mMMNNo.                   \n" + \
+"                                    `oNMMMMs-                   \n" + \
+"                                    .oNMMMMo.                   \n" + \
+"                                   -odNMMho/.                   \n" + \
+"                                  :yMMMMM+.                     \n" + \
+"                                  .sNMMMMMy-                    \n" + \
+"                                  `dNMMMMMMm/`                  \n" + \
+"                                 `+NMMMMMMMN+``   ``            \n" + \
+"                                 `/NMMMMMMMMdyso+oyy`           \n" + \
+"                                  -NMMMMMMMMMMMMMNys.           \n" + \
+"                                  -NMMMMMMMMMMM--/.             \n" + \
+"                                 .hMMMMMMMNo:---:`              \n"
+
+frame2 = \
+"                              Straighten your neck!!!           \n" + \
+"                                          .                     \n" + \
+"                                        `:++/:.                 \n" + \
+"                                       `+mMMNNo.                \n" + \
+"                                       `oNMMMMs-                \n" + \
+"                                       .oNMMMMo.                \n" + \
+"                                     -odNMMho/.                 \n" + \
+"                                    :yMMMMM+.                   \n" + \
+"                                   .sNMMMMMy-                   \n" + \
+"                                  `dNMMMMMMm/`                  \n" + \
+"                                 `+NMMMMMMMN+``   ``            \n" + \
+"                                 `/NMMMMMMMMdyso+oyy`           \n" + \
+"                                  -NMMMMMMMMMMMMMNys.           \n" + \
+"                                  -NMMMMMMMMMMM--/.             \n" + \
+"                                 .hMMMMMMMNo:---:`              \n"
+ 
+frame3 = \
+"                              Straighten your neck!!!           \n" + \
+"                                              .                 \n" + \
+"                                           `:++/:.              \n" + \
+"                                         `+mMMNNo.              \n" + \
+"                                        `oNMMMMs-               \n" + \
+"                                        .oNMMMMo.               \n" + \
+"                                      -odNMMho/.                \n" + \
+"                                    :yMMMMM+.                   \n" + \
+"                                   .sNMMMMMy-                   \n" + \
+"                                  `dNMMMMMMm/`                  \n" + \
+"                                 `+NMMMMMMMN+``   ``            \n" + \
+"                                 `/NMMMMMMMMdyso+oyy`           \n" + \
+"                                  -NMMMMMMMMMMMMMNys.           \n" + \
+"                                  -NMMMMMMMMMMM--/.             \n" + \
+"                                 .hMMMMMMMNo:---:`              \n"
+
+frame4 = \
+"                              Straighten your neck!!!           \n" + \
+"                                            .                   \n" + \
+"                                          `:++/:.               \n" + \
+"                                        `+mMMNNo.               \n" + \
+"                                       `oNMMMMs-                \n" + \
+"                                      .oNMMMMo.                 \n" + \
+"                                   -odNMMho/.                   \n" + \
+"                                  :yMMMMM+.                     \n" + \
+"                                  .sNMMMMMy-                    \n" + \
+"                                  `dNMMMMMMm/`                  \n" + \
+"                                 `+NMMMMMMMN+``   ``            \n" + \
+"                                 `/NMMMMMMMMdyso+oyy`           \n" + \
+"                                  -NMMMMMMMMMMMMMNys.           \n" + \
+"                                  -NMMMMMMMMMMM--/.             \n" + \
+"                                 .hMMMMMMMNo:---:`              \n"
+frame5 = \
+"                              Straighten your neck!!!           \n" + \
+"                                          .                     \n" + \
+"                                         `:++/:.                \n" + \
+"                                         `+mMMNNo.              \n" + \
+"                                        `oNMMMMs-               \n" + \
+"                                       .oNMMMMo.                \n" + \
+"                                     -odNMMho/.                 \n" + \
+"                                    :yMMMMM+.                   \n" + \
+"                                   .sNMMMMMy-                   \n" + \
+"                                  `dNMMMMMMm/`                  \n" + \
+"                                 `+NMMMMMMMN+``   ``            \n" + \
+"                                 `/NMMMMMMMMdyso+oyy`           \n" + \
+"                                  -NMMMMMMMMMMMMMNys.           \n" + \
+"                                  -NMMMMMMMMMMM--/.             \n" + \
+"                                 .hMMMMMMMNo:---:`              \n"
+
+frame6 = \
+"                              Straighten your neck!!!           \n" + \
+"                                          .                     \n" + \
+"                                        `:++/:.                 \n" + \
+"                                       `+mMMNNo.                \n" + \
+"                                       `oNMMMMs-                \n" + \
+"                                       .oNMMMMo.                \n" + \
+"                                     -odNMMho/.                 \n" + \
+"                                    :yMMMMM+.                   \n" + \
+"                                   .sNMMMMMy-                   \n" + \
+"                                  `dNMMMMMMm/`                  \n" + \
+"                                 `+NMMMMMMMN+``   ``            \n" + \
+"                                 `/NMMMMMMMMdyso+oyy`           \n" + \
+"                                  -NMMMMMMMMMMMMMNys.           \n" + \
+"                                  -NMMMMMMMMMMM--/.             \n" + \
+"                                 .hMMMMMMMNo:---:`              \n"
+
+frame7 = \
+"                              Straighten your neck!!!           \n" + \
+"                                       .                        \n" + \
+"                                     `:++/:.                    \n" + \
+"                                    `+mMMNNo.                   \n" + \
+"                                    `oNMMMMs-                   \n" + \
+"                                    .oNMMMMo.                   \n" + \
+"                                   -odNMMho/.                   \n" + \
+"                                  :yMMMMM+.                     \n" + \
+"                                  .sNMMMMMy-                    \n" + \
+"                                  `dNMMMMMMm/`                  \n" + \
+"                                 `+NMMMMMMMN+``   ``            \n" + \
+"                                 `/NMMMMMMMMdyso+oyy`           \n" + \
+"                                  -NMMMMMMMMMMMMMNys.           \n" + \
+"                                  -NMMMMMMMMMMM--/.             \n" + \
+"                                 .hMMMMMMMNo:---:`              \n"
+
+frames = [frame1, frame2, frame3, frame4, frame5, frame6, frame7]
---
+++ data_server/terminal_animation.py
@@ -0,0 +1,35 @@
+import time
+import os
+import animation
+
+threshold = 45
+th = threshold * (-1)
+
+duration = 0.125    # The duration of time in seconds between each frame.
+cycles = 10       # The number of cycles the animation plays through.
+
+def animate():
+    """Iterate through the frames, printing then clearing each one to create an animation."""
+    count = 0
+    while count < cycles:
+        for frame in animation.frames:
+            print '\033[1;34m' + frame + '\033[1;m'  # Print the frame in color blue.
+            time.sleep(duration)
+            print(os.system('clear'))
+        count = count + 1
+
+# animate()
+
+while True:
+
+    xx = i[0]
+    yy = i[1]
+    cnt = 0
+
+    for i in row['corrected']:
+        if (yy > th || xx > threshold || yy < th):
+            cnt += 1
+            if (cnt == 500000):
+                animate()
+        else:
+            cnt = 0
